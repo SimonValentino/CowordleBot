d7a13c75998fc9ca4cc32883b0e89d1c881e83f7
Simon Valentino <simontvalentino@gmail.com>
2023-08-25 16:15:11 -0400

    Started getting the wordle bot website

--- bot.py
+++ bot.py
@@ -4,14 +4,22 @@ from selenium.webdriver.common.by import By
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 
-driver=webdriver.Chrome(options=options)
-driver.get("https://cowordle.org/mode-2")
+game = webdriver.Chrome(options=options)
+game.get("https://cowordle.org/mode-2")
 
-hard_mode = driver.find_element(By.CLASS_NAME, "difficulty_hard_btn")
-hard_mode.click()
+game.find_element(By.CLASS_NAME, "difficulty_hard_btn").click()
+game.find_element(
+    By.XPATH,
+    "/html/body/div[1]/div/section/div/div[1]/div/div/div/div/div[5]/div[2]/div/div[1]/div[3]/div/input").send_keys("GorillaGamer29")
+game.find_element(By.CLASS_NAME, "start_btn").click()
 
-name = driver.find_element(By.XPATH, "/html/body/div[1]/div/section/div/div[1]/div/div/div/div/div[5]/div[2]/div/div[1]/div[3]/div/input")
-name.send_keys("GorillaGamer29")
 
-start = driver.find_element(By.CLASS_NAME, "start_btn")
-start.click()
+
+# THIS CODE will be used in the future for making the bot not visible
+# options.add_argument("--headless")
+# options.add_argument("window-size=1000,300")
+
+bot = webdriver.Chrome(options=options)
+bot.get("https://www.simn.me/eldrow/")
+
+bot.find_element(By.ID, "easy").click()
