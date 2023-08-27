9d18ced4f0c8ae98f89a22d6d0b745f6e3351cd2
Simon Valentino <simontvalentino@gmail.com>
2023-08-27 15:43:14 -0400

    Needed more time sleeping.

--- bot.py
+++ bot.py
@@ -100,7 +100,7 @@ while True:
     ).get_attribute("class") == "Row Row-locked-in":
         continue
     
-    time.sleep(0.07)
+    time.sleep(0.1)
 
     hints = [game.find_element(
         By.XPATH, f"/html/body/div[1]/div/section/div/div[1]/div/div/div/div/div[2]/div[1]/div/div[1]/div/div[1]/div[{i}]/div[{j}]") for j in range(1, NUM_LETTERS + 1)]
