2cb0ec4bb0f0d174ac7be677ab54487478a7ebd4
Simon Valentino <simontvalentino@gmail.com>
2023-09-02 12:43:45 -0400

    Renamed some things.

---
+++ word_reservoir.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def get_answer_list():
+    words = np.array([])
+    with open("wordle_answers.txt", "r") as file:
+        for line in file:
+            words.
+    return words
+
+def get_guess_list():
+    words = np.array([])
+    with open("guess_list.py", "r") as file:
+        for line in file:
+            words.append
